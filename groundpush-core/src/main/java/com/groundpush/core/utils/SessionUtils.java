dc79079001db292bddc8402507aae51f38a33b3a
zhangxinzhong <zhangxz_email@163.com>
2019-09-10 17:11:17 +0800

    添session utils

---
+++ groundpush-core/src/main/java/com/groundpush/core/utils/SessionUtils.java
@@ -0,0 +1,30 @@
+package com.groundpush.core.utils;
+
+import com.groundpush.core.model.LoginUserInfo;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+/**
+ * @description: 获取登录用户
+ * @author: zhangxinzhong
+ * @date: 2019-09-10 下午5:04
+ */
+@Component
+public class SessionUtils {
+
+    /**
+     * 获取登录用户session
+     * @return
+     */
+    public Optional<LoginUserInfo> getLoginUserInfo() {
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
+        LoginUserInfo loginUserInfo = (LoginUserInfo) session.getAttribute(Constants.SESSION_LOGIN_USER_INFO);
+        return loginUserInfo == null ? Optional.empty() : Optional.of(loginUserInfo);
+
+    }
+
+}
