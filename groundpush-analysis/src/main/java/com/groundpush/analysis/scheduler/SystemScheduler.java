e88714b764bfb272621e5d549a49eb7f6dc607a5
zhangxinzhong <zhangxz_email@163.com>
2019-09-27 17:38:50 +0800

    渠道数据导入

--- groundpush-analysis/src/main/java/com/groundpush/analysis/scheduler/SystemScheduler.java
+++ groundpush-analysis/src/main/java/com/groundpush/analysis/scheduler/SystemScheduler.java
@@ -2,14 +2,17 @@ package com.groundpush.analysis.scheduler;
 
 import com.groundpush.analysis.service.ChannelDataService;
 import com.groundpush.analysis.service.ChannelExcelService;
+import com.groundpush.analysis.service.OrderService;
 import com.groundpush.core.condition.ChannelDataQueryCondition;
 import com.groundpush.core.model.ChannelData;
 import com.groundpush.core.model.ChannelExcel;
+import com.groundpush.core.model.Order;
 import com.groundpush.core.utils.Constants;
 import com.groundpush.core.utils.DateUtils;
 import com.groundpush.core.utils.ExcelTools;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.aspectj.weaver.ast.Or;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +26,8 @@ import java.text.MessageFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 /**
  * @author luzq
@@ -39,14 +44,14 @@ public class SystemScheduler {
     @Value("${groundpush.channel.mapping.filed:personnelId,produceTime,isEffective,failureResult}")
     private String channelMappingFiled;
 
-    @Value("${groundpush.virt.user.id:1}")
-    private int virtUserId;
-
     @Resource
     private ChannelDataService channelDataService;
 
     @Resource
     private ChannelExcelService channelExcelService;
+
+    @Resource
+    private OrderService orderService;
 
     private long count = 0L;
 
@@ -55,59 +60,77 @@ public class SystemScheduler {
         count = 0;
         String startTime = LocalDateTime.now().toString();
         DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        List<ChannelExcel> channelExcels = channelExcelService.queryChannelExcelAll();
 
-        List<ChannelExcel> cdmArray = channelExcelService.queryChannelExcelAll();
-        if (cdmArray.size() > 0) {
+        if (channelExcels.size() > 0) {
             ExcelTools excelTools = ExcelTools.getInstance();
-            cdmArray.forEach(cdm -> {
+            for (ChannelExcel channelExcel : channelExcels) {
                 try {
-                    String path = rootPath + File.separator + cdm.getFileName();
+                    Map<String, Order> orderMap = orderService.queryOrderByTaskIdReturnMap(channelExcel.getTaskId());
+                    log.info("订单数：{}", orderMap.size());
+                    String path = rootPath + File.separator + channelExcel.getFileName();
                     excelTools.openExcel(path);
                     final Object[] title = excelTools.getExcelTitle();
-                    final String mapping = cdm.getMapping();
+                    final String mapping = channelExcel.getMapping();
                     List<ChannelData> cds = new ArrayList<>();
-                    excelTools.setRowResult(100, (sheetName, countRow, resultCount, objects) -> {
-                        objects.forEach(obj -> {
+                    List<Order> existOrder = new ArrayList<>();
+
+                    excelTools.setRowResult(1000, (sheetName, countRow, resultCount, objects) -> {
+
+                        for (Object[] obj : objects) {
+
                             Object[] excelRowData = obj;
                             if (!StringUtils.equals(String.valueOf(excelRowData[0]), String.valueOf(title[0]))) {
                                 Map<String, Object> analysisResult = analysisSingletData(obj, mapping);
                                 String uniqueCode = String.valueOf(analysisResult.get("uniqueCode"));
                                 String failureResult = String.valueOf(analysisResult.get("failureResult"));
                                 String isEffective = String.valueOf(analysisResult.get("isEffective"));
-                                Integer resStatus = Constants.XLS_IS_EFFECTIVE_VAILD_TEXT.equals(isEffective) ? Constants.XLS_IS_EFFECTIVE_VAILD : Constants.XLS_IS_EFFECTIVE_INVAILD;
-                                boolean isExistOrder = true;
-                                if (channelDataService.updateOrderStatus(uniqueCode, resStatus, failureResult) <= 0) {
-                                    isExistOrder = false;
-                                }
-                                //若渠道数据存在不处理
-                                Optional<ChannelData> channelDataOptional = channelDataService.getChannelDataByUniqueCode(ChannelDataQueryCondition.builder().channelId(cdm.getChannelId()).taskId(cdm.getTaskId()).uniqueCode(uniqueCode).build());
-                                if (!channelDataOptional.isPresent()) {
-                                    try {
-                                        ChannelData cd = ChannelData.builder().channelId(cdm.getChannelId()).taskId(cdm.getTaskId())
-                                                .uniqueCode(uniqueCode)
-                                                .isEffective(Constants.XLS_IS_EFFECTIVE_VAILD_TEXT.equals(isEffective)?Boolean.TRUE:Boolean.FALSE)
-                                                .description(String.valueOf(analysisResult.get("failureResult")))
-                                                .channelTime(LocalDateTime.parse(String.valueOf(analysisResult.get("produceTime")),df))
-                                                .isExistOrder(isExistOrder).build();
-                                        cds.add(cd);
-                                    } catch (Exception e) {
-                                        log.error(e.toString(), e);
+                                boolean isExistOrder = false;
+                                Order order = orderMap.get(uniqueCode);
+                                if (order != null) {
+                                    if (orderMap.containsKey(uniqueCode)) {
+                                        isExistOrder = true;
                                     }
-                                    count++;
+                                    order.setSettlementStatus(isExistOrder ? Constants.ORDER_STATUS_SUCCESS : Constants.ORDER_STATUS_REVIEW_FAIL);
+                                    order.setRemark(failureResult);
+                                    existOrder.add(order);
                                 }
+                                log.info("数据文件:{},count:{},参数：uniqueCode:{},isExistOrder:{},produceTime:{},isEffective:{},failureResult:{}", path,count,uniqueCode, isExistOrder, analysisResult.get("produceTime"), isEffective, failureResult);
+                                try {
+                                    ChannelData cd = ChannelData.builder()
+                                            .channelId(channelExcel.getChannelId())
+                                            .taskId(channelExcel.getTaskId())
+                                            .uniqueCode(uniqueCode)
+                                            .description(failureResult)
+                                            .isExistOrder(isExistOrder)
+                                            .isEffective(Constants.XLS_IS_EFFECTIVE_VAILD_TEXT.equals(isEffective) ? Boolean.TRUE : Boolean.FALSE)
+                                            .channelTime(LocalDateTime.parse(String.valueOf(analysisResult.get("produceTime")), df)).build();
+                                    cds.add(cd);
+                                } catch (Exception e) {
+                                    log.error(e.toString(), e);
+                                }
+                                count++;
                             }
-                        });
+                        }
+                        // 保存渠道数据
                         channelDataService.addChannelData(cds);
-                    });
-                    channelExcelService.updateChannelExcel(cdm);
-                } catch (Exception e) {
-                    log.error(MessageFormat.format("发生异常，详细：{0}", e.getMessage()));
-                }
+                        cds.clear();
 
-            });
+                        // 保存订单数据
+                        orderService.updateOrder(existOrder);
+                        existOrder.clear();
+
+                    });
+
+                } catch (Exception e) {
+                    log.error(e.toString(), e);
+                } finally {
+                    channelExcelService.updateChannelExcel(channelExcel);
+                }
+            }
 
         }
-        log.info(MessageFormat.format("处理完成:开始时间：{0}，结束时间：{1}，文件数量：{2}，总条数：{3}", startTime, LocalDateTime.now(), cdmArray.size(), count));
+        log.info(MessageFormat.format("处理完成:开始时间：{0}，结束时间：{1}，文件数量：{2}，总条数：{3}", startTime, LocalDateTime.now(), channelExcels.size(), count));
     }
 
 
--- groundpush-analysis/src/main/java/com/groundpush/analysis/service/OrderService.java
+++ groundpush-analysis/src/main/java/com/groundpush/analysis/service/OrderService.java
@@ -1,9 +1,39 @@
 package com.groundpush.analysis.service;
 
+import com.groundpush.core.model.Order;
+
+import java.util.List;
+import java.util.Map;
+
 /**
- * @description:
+ * @description: 订单
  * @author: zhangxinzhong
  * @date: 2019-09-27 上午11:02
  */
 public interface OrderService {
+
+    /**
+     * 通过任务编号查询订单
+     *
+     * @param taskId
+     * @return
+     */
+    List<Order> queryOrderByTaskId(Integer taskId);
+
+    /**
+     * 通过任务编号查询订单
+     *
+     * @param taskId
+     * @return
+     */
+    Map<String, Order> queryOrderByTaskIdReturnMap(Integer taskId);
+
+    /**
+     * 修改订单渠道状态 及 备注
+     *
+     * @param existOrder
+     * @param existOrder
+     * @return
+     */
+    Integer updateOrder(List<Order> existOrder);
 }
