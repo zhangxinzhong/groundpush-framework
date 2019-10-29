86a441f3f98f9003ecf96d880ca03f503eff7ad0
cedric <307056200@qq.com>
2019-10-29 14:09:03 +0800

    跳转链接bug修改

--- groundpush-app/src/main/java/com/groundpush/mapper/TaskCollectMapper.java
+++ groundpush-app/src/main/java/com/groundpush/mapper/TaskCollectMapper.java
@@ -44,9 +44,8 @@ public interface TaskCollectMapper {
             //今日您剩余推广次数
             " (SELECT ls.handler_num-count(1) FROM t_order a LEFT JOIN t_order_task_customer b ON a.order_id = b.order_id WHERE a.type = 2  AND b.customer_id = #{customerId} AND DATE_FORMAT(a.created_time, '%Y-%m-%d') = DATE_FORMAT(now(), '%Y-%m-%d') AND b.task_id = ls.task_id ) sur_pop_count, ",
             //任务参与人
-            " (SELECT count(1) FROM  t_order_task_customer  a LEFT JOIN  t_order b ON a.order_id = b.order_id WHERE b.type = 2 AND a.task_id = ls.task_id AND DATE_FORMAT(b.created_time, '%Y-%m-%d') = DATE_FORMAT(now(), '%Y-%m-%d')) task_person, ",
-            //查询任务关联的所有次要标签  格式为：label1,label2,label3,label4
-            " (SELECT  GROUP_CONCAT(b.label_name) FROM t_label b LEFT JOIN t_task_label c on b.label_id = c.label_id where b.type = 0 and c.task_id = ls.task_id) label_name",
+            " (SELECT count(1) FROM  t_order_task_customer  a LEFT JOIN  t_order b ON a.order_id = b.order_id WHERE b.type = 2 AND a.task_id = ls.task_id AND DATE_FORMAT(b.created_time, '%Y-%m-%d') = DATE_FORMAT(now(), '%Y-%m-%d')) task_person ",
+
             " from ",
             " ( ",
                 //location不为空时 查询所有未设置任务位置的任务
--- groundpush-core/src/main/java/com/groundpush/core/mapper/SpecialTaskMapper.java
+++ groundpush-core/src/main/java/com/groundpush/core/mapper/SpecialTaskMapper.java
@@ -168,8 +168,7 @@ public interface SpecialTaskMapper {
                 " (SELECT t.handler_num-count(1) FROM t_order a LEFT JOIN t_order_task_customer b ON a.order_id = b.order_id WHERE a.type = 2 AND b.customer_id = #{customerId} AND DATE_FORMAT(a.created_time, '%Y-%m-%d') = DATE_FORMAT(now(), '%Y-%m-%d') AND b.task_id = t.task_id ) sur_pop_count, ",
                 //任务参与人
                 " (SELECT count(1) FROM  t_order_task_customer  a LEFT JOIN  t_order b ON a.order_id = b.order_id WHERE b.type = 2 AND a.task_id = t.task_id AND DATE_FORMAT(b.created_time, '%Y-%m-%d') = DATE_FORMAT(now(), '%Y-%m-%d')) task_person, ",
-                //查询任务关联的所有次要标签  格式为：label1,label2,label3,label4
-                " (SELECT  GROUP_CONCAT(b.label_name) FROM t_label b LEFT JOIN t_task_label c on b.label_id = c.label_id where b.type = 0 and c.task_id = t.task_id) label_name,",
+
                 //判断是否为礼品 即特殊任务 has_special_task=1：特殊任务 has_special_task=0：非特殊任务
                 "		(CASE WHEN",
                            //第1步.当前客户为团队一员 且此与特殊任务关联  为true则执行第1.1步 为false则执行第1.2步
